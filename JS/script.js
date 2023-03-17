4899d48cf427311854c50dcd900f67ff9e405c9a
CosimoTramonte <cosimotramonte7@gmail.com>
2023-03-17 14:47:48 +0100

    first commit:  logic behind the program

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+/*
+
+Chiedi all’utente: il suo nome, il suo cognome ed il suo colore preferito. Il programma restituisce poi tutte le informazioni.
+
+1) chiedere tramite un prompt e salvare il nome in una variante
+2) chiedere tramite un prompt e salvare il cognome in una variante
+3) chiedere tramite un prompt e salvare il colore preferito in una variante
+4) stampare in pagina l'output
+5) possibili controlli per ogni passaggio con "console.log"
+
+*/
